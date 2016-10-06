d1af443c081cb84f85c1abd880add68c77c5acbb
Alan Spiewak <spiewak@infionline.net>
2016-10-06 11:22:05 -0400

    feat: Increase to 5 cats, reorg catframe structure

--- js/catscript.js
+++ js/catscript.js
@@ -3,7 +3,13 @@ var catList = [
 	{name: 'Diana',
 	 src: 'images/kitty.jpg'},
 	{name: 'Gemini',
-	 src: 'images/kitty2.jpg'}
+	 src: 'images/kitty2.jpg'},
+	{name: 'Harry',
+	 src: 'images/harry.jpg'},
+	{name: 'Hermione',
+	 src: 'images/hermione.jpg'},
+	{name: 'Kate',
+	 src: 'images/kate.jpg'}
 ];
 
 /* Object creation for a Cat object */
@@ -11,7 +17,7 @@ function Cat(name, imageSrc, index) {
 	this.name = name;			/* Name of the cat */
 	this.imageSrc = imageSrc;	/* Relative filename of image */
 	this.index = index;			/* Unique reference # for each cat */
-	var spanId = 'counter' + index;	/* String, name of this cat's counter span */
+	this.spanId = 'counter' + index;	/* String, name of this cat's counter span */
 
 	/* Row container where everything gets added */
 	var catRow = $('#catrow');
@@ -27,21 +33,21 @@ function Cat(name, imageSrc, index) {
 		'class': 'catframe col-md-5'
 	}).appendTo(catRow);
 
+	catDispCount = $('<div class="cattext col-md-9"><strong>' + name +
+		'</strong> Click count: <span id="' + this.spanId +
+		'"> -1</span></div>').appendTo(catFrame);
+
 	catImage = $('<img>', {
-		'class': 'catimage img-responsive',
+		'class': 'catimage img-responsive col-md-3',
 		'src': imageSrc,
 		'alt': 'Cat picture'
 	}).appendTo(catFrame);
-
-	catDispCount = $('<p class="counter"><strong>' + name +
-		'</strong> Click count: <span id="' + spanId +
-		'"> -1</span></p>').appendTo(catFrame);
 
 	/*	Define counter function & use it to initialize the numeric counter.
 		Note that the count is stored in the text of a span whose ID is of the
 		form counterN, where N is the index of the individual Cat.
 	 */
-	var catCountSpan = $('#' + spanId);
+	var catCountSpan = $('#' + this.spanId);
 	function incrementCount() {
 		var n = catCountSpan.text();
 		catCountSpan.text(++n);
