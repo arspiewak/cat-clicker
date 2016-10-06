e4503edeeac8422f84b42d16859a454bc1301500
Alan Spiewak <spiewak@infionline.net>
2016-10-06 00:58:03 -0400

    feat: Display two cats; count clicks for each
    Modified directory structure for project, added bootstrap framework and
    jQuery library to manage changes.

--- js/catscript.js
+++ js/catscript.js
@@ -1,15 +1,65 @@
-function Clicker () {
-	var clickCount = 0;
-	var $currCount = $('#currCount');
-	$currCount.text(clickCount);
+/* List of cat objects to be created onscreen */
+var catList = [
+	{name: 'Diana',
+	 src: 'images/kitty.jpg'},
+	{name: 'Gemini',
+	 src: 'images/kitty2.jpg'}
+];
 
-	function clickHandler() {
-		$currCount.text(++clickCount);
+/* Object creation for a Cat object */
+function Cat(name, imageSrc, index) {
+	this.name = name;			/* Name of the cat */
+	this.imageSrc = imageSrc;	/* Relative filename of image */
+	this.index = index;			/* Unique reference # for each cat */
+	var spanId = 'counter' + index;	/* String, name of this cat's counter span */
+
+	/* Row container where everything gets added */
+	var catRow = $('#catrow');
+
+	/* jQuery objects to add to DOM */
+	var catFrame;				/* Holds all this cat's info */
+	var catImage;				/* Picture of the cat */
+	var catDispCount;			/* Display of click count */
+
+	/* Create Kitty's presence */
+
+	catFrame = $('<div></div>', {
+		'class': 'catframe col-md-5'
+	}).appendTo(catRow);
+
+	catImage = $('<img>', {
+		'class': 'catimage img-responsive',
+		'src': imageSrc,
+		'alt': 'Cat picture'
+	}).appendTo(catFrame);
+
+	catDispCount = $('<p class="counter"><strong>' + name +
+		'</strong> Click count: <span id="' + spanId +
+		'"> -1</span></p>').appendTo(catFrame);
+
+	/*	Define counter function & use it to initialize the numeric counter.
+		Note that the count is stored in the text of a span whose ID is of the
+		form counterN, where N is the index of the individual Cat.
+	 */
+	var catCountSpan = $('#' + spanId);
+	function incrementCount() {
+		var n = catCountSpan.text();
+		catCountSpan.text(++n);
 	}
 
-	$('#cat').click(clickHandler);
+	/* Install the click-counting callback function and call it to initialize */
+	catFrame.click(incrementCount);
+	catFrame.click();
 
 	return;
 }
 
-var clicker = new Clicker();
+/* Instantiate the kitties defined in catList once there's a place to put them. */
+document.addEventListener('DOMContentLoaded', function() {
+	for (var i = 0, len = catList.length; i < len; i++) {
+		var cat = catList[i];
+		/* catObj is added as an attribute to hold the object we create */
+		catList[i].catObj = new Cat (cat.name, cat.src, i);
+	}
+	return;
+});
